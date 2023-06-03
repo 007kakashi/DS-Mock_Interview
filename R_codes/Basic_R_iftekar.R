d18edb9f2f438e4e9288e006bfe0cddb330d5975
upendra gosavi <117889538+007kakashi@users.noreply.github.com>
2023-06-03 19:52:08 +0530

    Add files via upload

---
+++ R_codes/Basic_R_iftekar.R
@@ -0,0 +1,48 @@
+
+getwd()# to know where is your path
+
+d=read.csv('E:/P1-OfficeSupplies.csv') # to load the data from wd
+
+rm('P1_OfficeSupplies') # for removing the object or variabale
+
+a=5
+b<-4
+
+5->a
+
+a+b
+
+a-b
+
+a%%b
+
+a*b
+
+a^b
+ 
+sqrt(10)
+
+data=c(10,20,30) # vector is used to store multiple values in variable
+data
+
+data=c(10,20,30,'upendra') # we can store the string into vector
+data
+
+x=c(10,20,30)
+y=c(10,20,30)
+a=x+y
+a
+
+x[1] # indexing 
+
+x[1:3] # it gives me 1 to 3 index all values 
+
+y[-1] # it means ignore 1 index and give me all
+
+y[1]=90 # modifyg values using index number
+
+y<1 # gives you boolean values
+
+y[y<3]=9 # modifyng  all values in once
+
+
